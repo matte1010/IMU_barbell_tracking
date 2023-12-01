0d35fb8d3be45d9ba1ddd1c6228fed420de91d1e
tormodin <torm@kth.se>
2023-12-01 13:57:35 +0100

    testing plots

--- plots.py
+++ plots.py
@@ -19,3 +19,40 @@ root.destroy()
 # Load accelerometer data
 with open(file_path) as f:
     data = json.load(f)
+    # Assuming your JSON structure contains 'x' and 'y' arrays, modify as needed
+accx = data['accX']
+accy = data['accY']
+accz = data['accZ']
+xgyro = data['magX']
+ygyro = data['magY']
+zgyro = data['magZ']
+timestampsAcc = data['timeStampsAcc']
+timestampsGyro = data['timeStampsGyro']
+plt.figure(figsize=(10, 6))
+
+# Plotting accX
+plt.subplot(3, 1, 1)  # Creating subplot for accX
+plt.plot(timestampsAcc, accx, label='accX')
+plt.xlabel('Timestamps')
+plt.ylabel('Acceleration (accX)')
+plt.title('Acceleration X')
+plt.legend()
+
+# Plotting accY
+plt.subplot(3, 1, 2)  # Creating subplot for accY
+plt.plot(timestampsAcc, accy, label='accY')
+plt.xlabel('Timestamps')
+plt.ylabel('Acceleration (accY)')
+plt.title('Acceleration Y')
+plt.legend()
+
+# Plotting accZ
+plt.subplot(3, 1, 3)  # Creating subplot for accZ
+plt.plot(timestampsAcc, accz, label='accZ')
+plt.xlabel('Timestamps')
+plt.ylabel('Acceleration (accZ)')
+plt.title('Acceleration Z')
+plt.legend()
+
+plt.tight_layout()  # Adjusting layout for subplots
+plt.show()
